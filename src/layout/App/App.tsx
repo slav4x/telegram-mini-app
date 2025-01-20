e8de6acc8fbfa92af980001fd30c6d2af7adef04
Slava Khripushin <slava.cpa@yandex.ru>
2025-01-20 21:59:00 +0700

    path

--- src/layout/App/App.tsx
+++ src/layout/App/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router';
 
-import { Header, Navbar } from '../../components';
-import { useTelegramSetup } from '../../hooks/useTelegramSetup';
+import { Header, Navbar } from '@/components';
+import { useTelegramSetup } from '@/hooks/useTelegramSetup';
 
 export const LayoutApp = () => {
 	const { isMobile } = useTelegramSetup();
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/'
+	base: '/',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src') // Устанавливаем @ как путь к папке src
+		}
+	}
 });
